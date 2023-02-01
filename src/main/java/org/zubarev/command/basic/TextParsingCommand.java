1c03f698e1c90223c77fcffa51c2f8c29d4f2556
Artur Zubarev <arturzubarevvrn@gmail.com>
2023-02-02 01:18:25 +0300

    с хэшмапой решено, нужно было создать и проинициализировать мапу в классе storage и вернуть ее геттером

--- src/main/java/org/zubarev/command/basic/TextParsingCommand.java
+++ src/main/java/org/zubarev/command/basic/TextParsingCommand.java
@@ -3,24 +3,26 @@ package org.zubarev.command.basic;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.zubarev.storage.Storage;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class TextParsingCommand {
 
-    Storage storage;
-    Update update;
+    private Storage storage = new Storage();
 
     //надо определиться с логикой работы hashmap и как грамотно получить значения по ключу
     public String parseMessageText(String message) {
-        String response;
-        switch (message) {
-            case ("/start"):
-                response = "hello";
-                break;
-            default:
-                response = "null";
+        String response = "";
+        for (Map.Entry<String,String> entry :
+            storage.getStorageMap().entrySet()){
+            if (entry.getKey().equalsIgnoreCase(message)){
+                response = entry.getValue();
+            }
 
+        }
+        if (message.equalsIgnoreCase("/start")){
+            response = "hi!";
         }
         return response;
     }
--- src/main/java/org/zubarev/storage/Storage.java
+++ src/main/java/org/zubarev/storage/Storage.java
@@ -6,20 +6,23 @@ import lombok.NoArgsConstructor;
 import java.util.HashMap;
 import java.util.Map;
 
-@Getter
+
 
 public class Storage {
-    private Map <String,String> storageMap;
-    public Storage() {
-          final Map<String, String> storageMap = new HashMap<>(Map.of(
-                "Мартин", "https://drive.google.com/file/d/1yC-5_Xq6nVNGbmi8oHCx9DpAI1Lx4_LL/view?usp=sharing",
-                "Блох", "https://drive.google.com/file/d/1yC-5_Xq6nVNGbmi8oHCx9DpAI1Lx4_LL/view?usp=sharing",
-                "Пейн", "https://drive.google.com/file/d/1ogl-Mbe142G7NYRYlJey4LGzg3ddKqhx/view?usp=sharing",
-                "Бхаргава", "https://drive.google.com/file/d/145b4_JJYzcteZgw-V42s1N7WMlr3PKyp/view?usp=sharing",
-                "Вопросы для собеседования", "https://drive.google.com/file/d/1ws7q7VFqxaPp_7TcuIxC-CEmrIsS--J7/view?usp=sharing",
-                "Паттерны", "https://drive.google.com/file/d/145b4_JJYzcteZgw-V42s1N7WMlr3PKyp/view?usp=sharing"));
+     private final Map <String,String> storageMap = new HashMap<>(Map.of(
+            "Мартин", "https://drive.google.com/file/d/1yC-5_Xq6nVNGbmi8oHCx9DpAI1Lx4_LL/view?usp=sharing",
+            "Блох", "https://drive.google.com/file/d/1yC-5_Xq6nVNGbmi8oHCx9DpAI1Lx4_LL/view?usp=sharing",
+            "Пейн", "https://drive.google.com/file/d/1ogl-Mbe142G7NYRYlJey4LGzg3ddKqhx/view?usp=sharing",
+            "Бхаргава", "https://drive.google.com/file/d/145b4_JJYzcteZgw-V42s1N7WMlr3PKyp/view?usp=sharing",
+            "Вопросы для собеседования", "https://drive.google.com/file/d/1ws7q7VFqxaPp_7TcuIxC-CEmrIsS--J7/view?usp=sharing",
+            "Паттерны", "https://drive.google.com/file/d/145b4_JJYzcteZgw-V42s1N7WMlr3PKyp/view?usp=sharing"));
+
+    public Map<String,String> getStorageMap() {
+        return storageMap;
     }
-}
+};
+
+
 
 
 
