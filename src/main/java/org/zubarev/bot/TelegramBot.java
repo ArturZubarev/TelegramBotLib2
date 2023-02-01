e12ae861e89d81794a5315e0189ba49162ac6219
Artur Zubarev <arturzubarevvrn@gmail.com>
2023-02-01 21:13:11 +0300

    работает корректно, дальще надо добавить хранилище в виде хэш мап

--- src/main/java/org/zubarev/bot/TelegramBot.java
+++ src/main/java/org/zubarev/bot/TelegramBot.java
@@ -1,24 +1,32 @@
 package org.zubarev.bot;
 
-import lombok.Getter;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.zubarev.command.basic.TextParsingCommand;
 
 public class TelegramBot extends TelegramLongPollingBot {
 
+    private TextParsingCommand textParsingCommand;
+    private String BOT_NAME = System.getenv("BOT_NAME");
+    private String BOT_TOKEN = System.getenv("BOT_TOKEN");
+
     @Override
     public String getBotToken() {
-        return "5932487136:AAEzwOc_trjPtZq8-lMAUIUh6tzueQ2J-rc";
+        return BOT_TOKEN;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()){
+        if (update.hasMessage() && update.getMessage().hasText()) {
             SendMessage message = new SendMessage();
             message.setChatId(update.getMessage().getChatId().toString());
-            message.setText("Test");
+            String inMessageText = update.getMessage().getText();
+            String response = new TextParsingCommand().parseMessageText(inMessageText);
+            message.setText(response);
+
             try {
                 execute(message);
             } catch (TelegramApiException e) {
@@ -31,6 +39,10 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "Developer_lib_bot";
+        return BOT_NAME;
     }
+
+
 }
+
+
